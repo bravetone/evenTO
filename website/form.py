58edfde1b2df9927f7f5143cca4c6fd451f4c449
Kaan Turkmen <kaanturkmen2000@gmail.com>
2022-01-12 20:00:11 -0500

    Fix edit event page existence issue

--- website/form.py
+++ website/form.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, FileField, SelectField, Form, DecimalField, RadioField
+from flask_login import current_user
+from wtforms import StringField, SubmitField, PasswordField, FileField, SelectField, Form, DecimalField, IntegerField
 from wtforms.validators import DataRequired,InputRequired, ValidationError, Email, EqualTo, Length, NumberRange
 from website.model import User, Event, Category, choice_query, role_query, music_query
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
@@ -101,17 +102,18 @@ class CategoryForm(FlaskForm):
 class EditProfileForm(FlaskForm):
     def validate_username(self, username_to_check):
         user = User.query.filter_by(username=username_to_check.data).first()
-        if user:
+        if user and current_user != user:
             raise ValidationError("User with name ' %s ' exist!" % self.username.data)
 
     def validate_mail(self, mail_to_check):
         mail = User.query.filter_by(mail=mail_to_check.data).first()
-        if mail:
+        if mail and current_user != mail:
             raise ValidationError('Email Address already exists! Please try a email address!')
 
-    name = StringField(label='Name:', validators=[DataRequired(), Length(min=2, max=20)])
-    family_name = StringField(label='Surname:', validators=[DataRequired(), Length(min=3, max=20)])
-    username = StringField(label='Username:', validators=[DataRequired(), Length(min=3, max=15)])
+    name = StringField(label='Name', validators=[DataRequired(), Length(min=2, max=20)])
+    family_name = StringField(label='Surname', validators=[DataRequired(), Length(min=3, max=20)])
+    username = StringField(label='Username', validators=[DataRequired(), Length(min=3, max=15)])
     mail = StringField(label='E-Mail', validators=[DataRequired(), Email()])
+    password = PasswordField(label='New Password')
     #password2 = PasswordField(label='Confirm Password:', validators=[EqualTo('password'), ])
     submit = SubmitField(label='Submit')
